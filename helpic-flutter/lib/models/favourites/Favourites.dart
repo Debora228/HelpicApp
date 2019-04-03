dfc7dd34e2882e306500c64ebcbb35f0d04ab955
Savyo Nascimento <savionascimento14@gmail.com>
2019-04-03 10:13:57 -0300

    Adicionando partes finais

---
+++ helpic-flutter/lib/models/favourites/Favourites.dart
@@ -0,0 +1,14 @@
+class Favourites {
+  int id;
+  String username;
+  String comments;
+  String downloads;
+  String likes;
+  String userImage;
+  String contentUrl;
+  String isVideo;
+  String tags;
+
+  Favourites(this.id, this.username, this.comments, this.downloads,
+      this.likes, this.userImage, this.contentUrl, this.isVideo, this.tags);
+}
